90336657acd7e469c5c1c28ee389f2054c5cfc03
op.op0102569@gmail.com <Abdo1399>
2020-09-02 12:26:56 +0200

    Fetching data

--- lib/controllers/authController.dart
+++ lib/controllers/authController.dart
@@ -1,11 +1,8 @@
-import 'dart:math';
-
 import 'package:get/get.dart';
 import 'package:shop_app/models/user.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'package:get/get.dart';
 
 class AuthController extends GetxController {
   UserModel userModel = UserModel();
